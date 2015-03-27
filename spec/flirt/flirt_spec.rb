bf77e7f454f0feb7896b4bb9e9f19aac9177e3a0
Ben Dunkley <bendunkley@fullsixuk.com>
2015-03-27 18:49:08 +0000

    Clean up main spec file

--- spec/flirt/flirt_spec.rb
+++ spec/flirt/flirt_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 require 'flirt/flirt_test_classes'
 
-describe "Flirt integration" do
-    before(:each) do
-        Flirt.enable
-    end
+describe Flirt do
 
     describe "with a single :pancake_fried subscriber" do
 
@@ -34,57 +31,3 @@ describe "Flirt integration" do
         end
     end
 end
-
-
-# A single point where events can be broadcast to and listeners can be registered.
-#
-# To publish:
-#     event_data = { fruit: "apple" }
-#     Flirt.broadcast :fruit_picked, event_data
-# Or:
-#     Flirt.publish :fruit_picked, event_data
-# (These are aliases of each other)
-#
-# To subscribe:
-#
-# class MyListener
-#     def initialize
-#         Flirt.subscribe self, :fruit_picked, with: :fruit_picked_callback
-#         # or the alias
-#         Flirt.listen self, :fruit_picked, with: :fruit_picked_callback
-#     end
-#
-#     def fruit_picked_callback(event_data)
-#         puts "The #{event_data[:fruit]} has been picked"
-#     end
-# end
-#
-# Syntactic sugar for subscription has been provided in the form of a module:
-#
-# class MyListener
-#     include FlirtListener
-#
-#     def initialize
-#         subscribe_to :fruit_picked, with: :fruit_picked_callback
-#         # or the alias
-#         listen_to :fruit_picked, with: :fruit_picked_callback
-#     end
-#
-#     def fruit_picked_callback(event_data)
-#         puts "The #{event_data[:fruit]} has been picked"
-#     end
-# end
-#
-# or even:
-#
-# class MyListener
-#     extend Flirt::Listener
-#
-#     subscribe_to :fruit_picked, with: :fruit_picked_callback
-#     # or the alias
-#     listen_to :fruit_picked, with: :fruit_picked_callback
-#
-#     def self.fruit_picked_callback(event_data)
-#         puts "The #{event_data[:fruit]} has been picked"
-#     end
-# end
